095a3e7789b2109686387033ae59636de287672f
lars kvn <kvnyarbis@gmail.com>
2023-07-14 20:39:48 -0500

    api

---
+++ src/main/java/com/smartheath/GatewayServiceApplication.java
@@ -0,0 +1,18 @@
+package com.smartheath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+
+
+}
